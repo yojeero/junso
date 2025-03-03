a609835b8f9e49caf723cc8a88b225107c60044e
Yojee <yojeero@gmail.com>
2025-03-03 22:08:13 +0300

    global update

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 <img src="preview/git.png">
 
-### [Junso](https://junso.vercel.app/) - landing HTML template
+### [Junso](https://junso.vercel.app/) - Tailwind v4 template
 
-- CSS Grid Layout
-- HTML5, CSS3  
-- jQuery  
-- SVG icons  
-- Google Fonts  
+- Tailwind CSS v4
+- Simple use Tailwind via CLI
+- HTML5, CSS3
+- Inline SVG icons 
+- Local Google Fonts
 - Yandex Maps
 - Fully responsive layout
+
+```
+Tailwind CSS v4 
+
+Don't need tailwind.config anymore.
+Just use > @import "tailwindcss"; < in your .css
+```
+
+Use Tailwind via CLI
+
+- in project folder run in terminal > npm install tailwindcss @tailwindcss/cli  
+- create input.css for your CSS styles
+- insert > @import "tailwindcss"; < in input.css  
+- run in terminal > npx @tailwindcss/cli -i ./css/input.css -o ./css/style.css --watch 
+- run again > in terminal > npx @tailwindcss/cli -i ./css/input.css -o ./css/style.css --watch < for build your production CSS
+- remove input.css for production, because all styles were added in the previous step
+- Enjoy!
---
+++ js/app.js
@@ -0,0 +1,64 @@
+// ScrollReveall
+window.sr = ScrollReveal();sr.reveal('.reveal');
+
+// Nav
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("nav-content").style.width = "0";
+	document.getElementById("close-nav").style.display = "none";
+
+	document.querySelectorAll("#nav-content .nav a").forEach(function(navLink) {
+		navLink.addEventListener("click", function() {
+			closeNav();
+		});
+	});
+});
+
+function openNav() {
+	if (window.innerWidth <= 768) {
+		document.getElementById("nav-content").style.width = "100%";
+	} else {
+		document.getElementById("nav-content").style.width = "50%";
+	}
+	document.getElementById("close-nav").style.display = "block";
+}
+
+function closeNav() {
+	document.getElementById("nav-content").style.width = "0";
+	document.getElementById("close-nav").style.display = "none";
+}
+
+// Yandex Maps 
+ymaps.ready(startmap);
+		var myMap, myplacemark;
+
+		function startmap() {
+			myMap = new ymaps.Map("map", {
+				center: [34.0675198,-118.4042346],
+				zoom: 9,
+				controls: ["zoomControl", "fullscreenControl"]
+			});
+			myplacemark = new ymaps.GeoObject({
+				geometry: {
+					type: "Point",
+					coordinates: [34.0675198,-118.4042346]
+				},
+				properties: {
+					balloonContentHeader: "<p class='text-lg font-medium'>Beverly Hills Stores</p>",
+					balloonContentBody: "<p class='text-gray-900 font-medium h-4 p-2'>Rodeo Dr, Beverly Hills, CA</p>"
+				}
+			}, {
+				iconLayout: "default#image",
+				iconImageHref: "images/pin.png",
+				iconImageSize: [70, 98],
+				iconImageOffset: [-30, -120],
+				iconContentOffset: [0]
+			});
+			myMap.geoObjects.add(myplacemark);
+			myMap.behaviors.disable("scrollZoom")
+		};
+
+
+		
+
+
+
