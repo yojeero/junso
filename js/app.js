0efa4a110d8058e95ed2dff7c2ac439cc9ce8c3d
Yojee <yojeero@gmail.com>
2025-03-05 19:16:51 +0300

    new update

--- js/app.js
+++ js/app.js
@@ -7,12 +7,18 @@ setTimeout(function () {
 window.sr = ScrollReveal();sr.reveal('.reveal');
 
 // Day-Night Switcher
-const toggle = document.querySelector("#toggle-day");
+const toggle = document.querySelector("#day-night");
 	const root = document.documentElement;
 
-	toggle.addEventListener("click", () => {
-		root.classList.toggle("NightMode");
-	});
+	if (toggle) {
+		toggle.addEventListener("click", () => {
+			if (root.classList.contains("NightMode")) {
+				root.classList.remove("NightMode");
+			} else {
+				root.classList.add("NightMode");
+			}
+		});
+	}
 
 // Nav
 document.addEventListener("DOMContentLoaded", function() {
